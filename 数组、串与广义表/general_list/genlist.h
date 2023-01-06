dec6fbda55fd357598da242fbae981283addfe32
huangzy1218 <2691386627@qq.com>
2023-01-06 18:39:55 +0800

    general list

--- 数组、串与广义表/general_list/genlist.h
+++ 数组、串与广义表/general_list/genlist.h
@@ -1,4 +1,17 @@
-#include "..\..\linear_list\sequence_list\SeqList.h"
+/**
+ * @file genlist.h
+ * @author Huang Z.Y.
+ * @brief 广义表(General Lists)是一种非连续性的数据结构，是线性表的一种推广。
+ * 即广义表中放松对表元素的原子限制，容许它们具有其自身结构。它被广泛的应用于人工智能等领域的表处理语言LISP语言中。
+ * @version 0.1
+ * @date 2023-01-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifndef GENLIST_H
+#define GENLIST_H
 #include <cassert>
 #include <cstdlib>
 #include <iostream>
@@ -7,281 +20,170 @@ using namespace std;
 // 广义表结点类定义
 template <class T>
 struct GenListNode {
-public:
-    GenListNode()
-    {
-        utype = 0;
-        tlink = NULL;
-        info.ref = 0;
-    }
-    GenListNode(GenListNode<T>& rl)
-    {
-        utype = rl.utype;
-        tlink = rl.tlink;
-        info = rl.info;
-    }
-
-    // private:
     int utype; // 标志域：0表示附加头结点，1表示子节点，2代表子表结点
     GenListNode<T>* tlink;
     union // 使用联合有效减少内存
     {
-        int ref;
         T value;
         GenListNode<T>* hlink;
-    } info; // 信息域：utype = 0，信息域存放引用计数，utype = 1，存放数值，utype = 2存放下一个表结点的地址
-};
-
-template <class T>
-struct Items // 子表结点
-{
-    int utype; // 标志域：0表示附加头结点，1表示子节点，2代表子表结点
-    union {
-        int ref;
-        T value;
-        GenListNode<T>* link;
-    } info; // 信息域：utype = 0，信息域存放
-    Items()
-    {
-        utype = 0;
-        info.ref = 0;
-    }
-    Items(Items<T>& rl)
-    {
-        utype = rl.utype;
-        info = rl.info;
-    }
+    }; // 信息域：utype = 0，信息域存放引用计数，utype = 1，存放数值，utype = 2存放下一个表结点的地址
 };
 
 template <class T>
 class GenList {
 public:
-    GenList();
-    ~GenList();
-    bool head(Items<T>& x); // 返回表头
-    bool tail(GenList<T>& lt); // 返回表尾
-    GenListNode<T>* getFirst(); // 返回第一个元素
-    GenListNode<T>* next(GenListNode<T>* elem); // 返回elem的直接后继
-    void copy(const GenList<T>& r); // 广义表的复制
-    int length(); // 获取表长
-    int depth(); // 获取非递归表的深度
-    template <class U>
-    friend istream& operator>>(istream& in, GenList<U>& l); // 输入运算符重载
-
-    // private:
-    GenListNode<T>* first; // 广义表头指针
-    GenListNode<T>* copy(GenListNode<T>* ls); // 复制一个ls非共享非递归表
-    int length(GenListNode<T>* ls); // 求以ls指示的广义表长度
-    int depth(GenListNode<T>* ls); // // 求以ls指示的广义表深度
-    bool equal(GenListNode<T>* s, GenListNode<T>* t); // 判断以s和t为表头的广义表是否相等
-    void remove(GenListNode<T>* ls); // 释放以ls为附加头节点的广义表
-    void createList(istream& in, GenListNode<T>*& ls, SeqList<T>& l1,
-        SeqList<GenListNode<T>*>& l2); // 从输入流对象输入广义表的字符串描述，建立一个带附加头结点的广义表结构
-    // 注意：广义表的所有表，无论那一层的子表，都带有一个附加头结点，空表也不例外
-};
-
-// 构造函数
-template <class T>
-GenList<T>::GenList()
-{
-    first = new GenListNode<T>();
-    if (first == NULL) {
-        cerr << "内存分配失败" << endl;
-    }
-};
-
-// 返回表头
-template <class T>
-bool GenList<T>::head(Items<T>& x)
-{
-    if (first->tlink == NULL) // 空表
+    GenList() // 构造函数
+        : first(nullptr)
     {
-        return false;
-    } else {
-        x.utype = first->tlink->utype;
-        x.info = first->tlink->info;
-        return true;
     }
-};
-
-// 返回表尾
-template <class T>
-bool GenList<T>::tail(GenList<T>& lt)
-{
-    if (first->tlink == NULL) {
-        return false;
-    } else // 若非空，返回广义表除头元素之外其他元素组成的表
+    void createGenList() { createGenList(first); } // 创建广义表
+    void createGenList(GenListNode<T>*& gNode); // 创建广义表
+    void traverse() // 遍历广义表
     {
-        lt.first->utype = 0;
-        lt.first->info.ref = 0;
-        lt.first->tlink = copy(first->tlink->tlink); // 去除头元素
+        traverse(first);
+        cout << ";" << endl;
+    }
+    void traverse(GenListNode<T>* gNode); // 遍历广义表
+    GenList<T> copy(); // 拷贝
+    void copy(GenListNode<T>*& dest, GenListNode<T>* src); // 拷贝
+    bool find(T value); // 查找
+    bool find(GenListNode<T>* gNode, T value, int& tag);
+    void output(); // 输出
+    void outputLevels(GenListNode<T>* gnode, int level); // 层次输出
+
+private:
+    GenListNode<T>* first;
+};
+
+// 建立广义表
+template <class T>
+void GenList<T>::createGenList(GenListNode<T>*& gNode)
+{
+    char ch;
+    cin >> ch;
+    if (ch == '#') {
+        gNode = nullptr;
+    } else if (ch == '(') {
+        gNode = new GenListNode<T>();
+        gNode->utype = 1;
+        createGenList(gNode->hlink);
+    } else {
+        gNode = new GenListNode<T>();
+        gNode->utype = 0;
+        gNode->value = ch;
+    }
+    cin >> ch; // 递归回来后能进行后续的读入
+    if (gNode == nullptr)
+        ;
+    else if (ch == ',') {
+        createGenList(gNode->tlink);
+    } else if ((ch == ')') || (ch == ';')) {
+        gNode->tlink = nullptr;
     }
 };
 
-// 获取第一个元素
+// 按格式输出广义表
 template <class T>
-GenListNode<T>* GenList<T>::getFirst()
+void GenList<T>::traverse(GenListNode<T>* gNode)
 {
-    if (first->tlink == NULL) {
-        return NULL;
+    if (gNode->utype == 1) {
+        cout << "(";
+        if (gNode->hlink == nullptr) {
+            cout << "#";
+        } else {
+            traverse(gNode->hlink);
+        }
+        cout << ")";
     } else {
-        return first->tlink;
+        cout << gNode->value;
     }
-};
-
-// 获取elem直接后继
-template <class T>
-GenListNode<T>* GenList<T>::next(GenListNode<T>* elem)
-{
-    if (elem->tlink == NULL) {
-        return NULL;
-    } else {
-        return elem->tlink;
+    if (gNode->tlink != nullptr) {
+        cout << ",";
+        traverse(gNode->tlink);
     }
 };
 
 // 复制
 template <class T>
-void GenList<T>::copy(const GenList<T>& r)
+void GenList<T>::copy(GenListNode<T>*& dest, GenListNode<T>* src)
 {
-    first = copy(r.first);
-};
-
-// 复制，私有函数，复制一个无共享子表的非递归表
-template <class T>
-GenListNode<T>* GenList<T>::copy(GenListNode<T>* ls)
-{
-    GenList<T>* q = NULL;
-    if (ls != NULL) {
-        q = new GenListNode<T>();
-        q->utype = ls->utype;
-        switch (ls->utype) {
-        case 0:
-            q->info.ref = ls->info.ref; // 附加头结点
-            break;
-        case 1:
-            q->info.ref = ls->info.vale; // 原子结点
-            break;
-        case 2:
-            q->info.ref = copy(ls->info.hlink); // 表结点
-            break;
+    if (src->utype == 1) {
+        dest = new GenListNode<T>();
+        dest->utype = 1;
+        if (src->hlink == nullptr) {
+            dest->hlink = nullptr;
+        } else {
+            copy(dest->hlink, src->hlink);
         }
-        q->tlink = copy(ls->tlink); // 处理同一层下一结点
-    }
-    return q;
-};
-
-// 求广义表的长度
-template <class T>
-int GenList<T>::length()
-{
-    return length(first);
-};
-
-// 求非递归广义表的长度（私有）
-template <class T>
-int GenList<T>::length(GenListNode<T>* ls)
-{
-    if (ls != NULL) {
-        return 1 + length(ls->tlink);
     } else {
-        return 0;
+        dest = new GenListNode<T>();
+        dest->utype = 0;
+        dest->value = src->value;
+    }
+    if (src->tlink == nullptr) {
+        dest->tlink = nullptr;
+    } else {
+        copy(dest->tlink, src->tlink);
     }
 };
 
-// 求表的深度
+// 拷贝
 template <class T>
-int GenList<T>::depth()
+GenList<T> GenList<T>::copy()
 {
-    return depth(first);
+    GenList<T> ret;
+    copy(ret.first, first);
+    return ret;
 };
 
-// 计算非递归广义表的深度（私有）
+// 查找
 template <class T>
-int GenList<T>::depth(GenListNode<T>* ls)
+bool GenList<T>::find(T value)
 {
-    if (ls->tlink == NULL) {
-        return 1; // 空表深度为1
-    }
-    GenListNode<T>* temp = ls->tlink;
-    int m = 0, n;
-    while (temp != NULL) {
-        if (temp->utype == 2) {
-            n = depth(temp->info.hlink); // 计算以该结点为头的广义表深度
-            if (m < n) {
-                m = n;
-            }
-        }
-        temp = temp->tlink;
-    }
-    return m + 1;
+    int tag = 0;
+    return find(first, value, tag);
 };
 
-// 判断以s和t为表头的广义表是否相等（私有）
+// 查找
 template <class T>
-bool equal(GenListNode<T>* s, GenListNode<T>* t)
+bool GenList<T>::find(GenListNode<T>* gNode, T value, int& tag)
 {
-    bool flag;
-    if (s->tlink == NULL && t->tlink == NULL) {
-        return true;
+    if (gNode->utype == 1) {
+        if (gNode->hlink != nullptr) {
+            find(gNode->hlink, value, tag);
+        }
+    } else {
+        if (gNode->value == value) {
+            tag = 1;
+        }
     }
-    if (s->tlink != NULL && t->tlink != NULL && s->tlink->utype == t->tlink->utype) {
-        if (s->tlink->utype == 1) {
-            flag = (s->tlink->info.value == t->tlink->info.value);
-        } else if (s->tlink->utype == 2) {
-            flag = equal(s->tlink->info.hlink, t->tlink->info.hlink);
+    if (gNode->tlink != nullptr) {
+        find(gNode->tlink, value, tag);
+    }
+    return tag == 1;
+};
+
+// 输出
+template <class T>
+void GenList<T>::output()
+{
+    outputLevels(first, 1);
+};
+
+// 输出层次
+template <class T>
+void GenList<T>::outputLevels(GenListNode<T>* gNode, int level)
+{
+    if (gNode->utype == 1) {
+        if (gNode->hlink != nullptr) {
+            outputLevels(gNode->hlink, level + 1);
         }
-        if (flag) {
-            return equal(s->tlink, t->tlink);
-        }
+    } else {
+        cout << gNode->value << " " << level << endl;
+    }
+    if (gNode->tlink != nullptr) {
+        outputLevels(gNode->tlink, level);
     }
 };
 
-// 建立广义表
-template <class T>
-void GenList<T>::createList(istream& in, GenListNode<T>*& ls, SeqList<T>& l1, SeqList<GenListNode<T>*>& l2)
-{
-    T chr;
-    in >> chr;
-    if (isalpha(chr) && isupper(chr) || chr == '(') // 大写字母或'('
-    {
-        ls = new GenListNode<T>();
-        ls->utype = 2; // 建立子表
-        if (isalpha(chr) && isupper(chr)) // 表名处理
-        {
-            int n = l1.length();
-            int m = l1.search(chr);
-            if (m != -1) {
-                GenListNode<T>* p = l2.locate(m);
-                p->ref++;
-            } else {
-                l1.insert(n, chr);
-                l2.insert(n, ls);
-            }
-            in >> chr;
-            if (chr != '(') {
-                exit(1); // 表名后为(
-            }
-        }
-        ls->info.hlink = new GenListNode<T>();
-        ls->info.hlink.utype = 0;
-        ls->info.hlink->ref = 1;
-        createList(in, ls->hlink->tlink, l1, l2);
-        createList(in, ls, l1, l2);
-    } else if (isalpha(chr) && islower(chr)) // 建立元素结点
-    {
-        ls = new GenListNode<T>();
-        ls->utype = 1;
-        ls->info.value = chr;
-        createList(in, ls, l1, l2);
-    } else if (chr == ',') // 建立后续结点
-    {
-        createList(in, ls->tlink, l1, l2);
-    } else if (chr == ')') // 链收尾
-    {
-        ls->tlink - NULL;
-    } else if (chr == '#') // 空表，链收尾
-    {
-        ls == NULL;
-    }
-};
+#endif
--- 数组、串与广义表/general_list/main.cpp
+++ 数组、串与广义表/general_list/main.cpp
@@ -1,11 +1,38 @@
+/**
+ * @file main.cpp
+ * @author Huang Z.Y.
+ * @brief a test of general list
+ * @version 0.1
+ * @date 2023-01-06
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
 #include "GenList.h"
 
 int main()
 {
     GenList<char> genList;
-    SeqList<char> listName;
-    SeqList<GenListNode<char>*> node;
-    genList.createList(cin, genList.first, listName, node);
+
+    // 创建广义表 a,(#),b,c,(d,(e));
+    genList.createGenList();
+
+    // 遍历输出
+    genList.traverse();
+
+    // 查找
+    char target = 'a';
+    if (genList.find(target)) {
+        cout << "查找到 " << target << endl;
+    } else {
+        cout << "未查找到 " << target << endl;
+    }
+
+    // 拷贝
+    GenList<char> demo = genList.copy();
+
+    // 层次输出
+    demo.output();
 
     return 0;
 }
